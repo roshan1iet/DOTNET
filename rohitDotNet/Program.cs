8c37adb2c90e3c637e4e1c128d19f13e69124f6a
Roshan <sroshan27@gmail.com>
2024-12-28 17:22:17 +0530

    Add files via upload

---
+++ rohitDotNet/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using System.Security.Cryptography.X509Certificates;
+
+namespace rohitDotNet
+{
+    internal class MyClass {
+        
+
+        public void Addition()
+        {
+
+                Console.WriteLine("Enter the number");
+
+                int x = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("enter the number");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Addition= " + (x + y));
+
+            }
+        }
+
+}
